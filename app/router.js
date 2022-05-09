022b6ecd6f34930891f5b314c94a26f72dfeecf4
Ahmad Saefudin <ahmaddin211056@gmail.com>
2022-05-09 22:42:58 +0700

    add packages, categories and sub categories route module

--- app/router.js
+++ app/router.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
-// const authentication = require('./middleware/authentication');
+const authentication = require('./middleware/authentication');
 
-const userRoute = require('./controllers/users');
+const userRoute             = require('./controllers/users');
+const packagesRoute         = require('./controllers/packages');
+const categoriesRoute       = require('./controllers/categories');
+const subcategoriesRoute    = require('./controllers/sub_categories');
 
 router.get('/',async (req,res)=>{
     res.send('API worked well')
 })
 
 router.use('/user',userRoute);
+router.use('/packages',packagesRoute);
+router.use('/categories',categoriesRoute);
+router.use('/categories/:parent_id',(req,res,next)=>{
+
+    req.parent_id = req.params.parent_id
+
+    next()
+},subcategoriesRoute);
 
 module.exports = router;
